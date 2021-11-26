f03c8cf015e6a9764cadedd51dccf0f9dfd4b3e3
Pankaj <pankaj200635@gmail.com>
2021-11-26 09:02:20 +0530

    Ui for calculator app

--- lib/main.dart
+++ lib/main.dart
@@ -22,18 +22,19 @@ class STForm extends StatefulWidget {
 class _SIFormState extends State<STForm> {
   var _currencies = ['Rupees', 'Dollars', 'Pounds'];
   final _minimumPadding = 5.0;
-  String dropdownValue = 'One';
+  String dropdownValue = 'Rupees';
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+      // resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text("Simple Interest Calculator"),
       ),
       body: Container(
         margin: EdgeInsets.all(_minimumPadding * 2),
-        child: Column(
+        child: ListView(
             children: [
             getImageAsset(),
 
@@ -66,46 +67,80 @@ class _SIFormState extends State<STForm> {
           ),
         ),
 
-        Row(
-          children: [
-           Expanded(child: TextField(
-          keyboardType: TextInputType.number,
-          decoration: InputDecoration(
-              labelText: 'Terms',
-              hintText: 'Time in years',
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.0)
-              )
+        Padding(
+          padding:  EdgeInsets.only(
+              top: _minimumPadding, bottom: _minimumPadding),
+          child: Row(
+            children: [
+             Expanded(child: TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: 'Terms',
+                hintText: 'Time in years',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0)
+                )
+            ),
+          )) ,
+
+          Container(
+            width: _minimumPadding*5,
           ),
-        )) ,
-        Expanded(child: DropdownButton<String>(
-          value: dropdownValue,
-          icon: const Icon(Icons.arrow_downward),
-          iconSize: 24,
-          elevation: 16,
-          style: const TextStyle(
-              color: Colors.deepPurple
-          ),
-          underline: Container(
-            height: 2,
-            color: Colors.deepPurpleAccent,
-          ),
-          onChanged: (String? newValue) {
-            setState(() {
-              dropdownValue = newValue!;
-            });
-          },
-          items: <String>['One', 'Two', 'Free', 'Four']
-              .map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-          })
-              .toList(),
-        )),
-        ],
+          Expanded(child: DropdownButton<String>(
+            value: dropdownValue,
+            // icon: const Icon(Icons.arrow_downward),
+            // iconSize: 24,
+            // elevation: 16,
+            // style: const TextStyle(
+            //     color: Colors.deepPurple
+            // ),
+            // underline: Container(
+            //   height: 2,
+            //   color: Colors.deepPurpleAccent,
+            // ),
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownValue = newValue!;
+              });
+            },
+            items: _currencies
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            })
+                .toList(),
+          )),
+          ],
       ),
+        ),
+
+              Padding(
+                padding:  EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: Row(
+                  children: [
+                    Expanded(child: ElevatedButton(
+                      child: Text('Calculate'),
+                      onPressed: (){},
+                    )
+                    ),
+                    Container(
+                      width: _minimumPadding*5,
+                    ),
+                    Expanded(child: ElevatedButton(
+                      child: Text('Reset'),
+                      onPressed: (){},
+                    )
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(_minimumPadding*2),
+                child: Text('Todo Text'),
+              )
 
 
       ],
